aba41a5cbc33e738d93db96b1f178515175c09c2
cristopherHurtado <cristopherhurtado@gmail.com>
2022-08-31 13:56:41 -0400

    ejercicios gia 1

---
+++ index.js
@@ -0,0 +1,106 @@
+//1 REALICE UN ALGORITMO Y UN DIAGRAMA DE FLUJO PARA SUMAR 3 NUMEROS//
+
+function sumar(){
+
+    let a = 3;
+    let b = 5;
+    let c = 8;
+
+    let respuesta = a + b + c;
+
+    console.log("1) La suma es: " + respuesta)
+}
+sumar();
+
+//2 REALICE UN ALGORITMO Y UN DIAGRAMA DE FLUJO PARA CALCULAR EL PERIMETRO DE UNA CIRCUNFERENCIA//
+
+function perimetro(){
+
+    let = 2;
+    let pi = 3.14;
+    let r = 18;
+
+    let respuesta = 2 * pi * r;
+
+    console.log("2) El perimetro es: " + respuesta + " cm");
+}
+perimetro();
+
+//3 REALICE UN ALGORITMO Y UN DIAGRAMA DE FLUJO PARA CALCULAR EL PRECIO DE UN PRODUCTO, INGRESANDO EL PRECIO POR KILO Y EL PESO DEL PRODUCTO//
+
+function precio(){
+
+    let kilo = 550;
+    let peso = 3;
+
+    let respuesta = kilo * peso;
+
+    console.log("3) El precio por kilo es: $"+kilo+" el peso del producto es: "+peso+" kilos y el precio total es: $"+respuesta);
+}
+precio();
+
+//4 REALICE UN ALGORITMO Y UN DIAGRAMA DE FLUJO PARA CALCULAR EL SUELDO DE UN EMPLEADO EN BASE A LA CANTIDAD DE HORAS TRABAJADAS Y EL VALOR POR HORA//
+
+function sueldo(){
+
+    let sueldo = 480000;
+    let horas = 180;
+
+    let respuesta = sueldo / horas;
+
+    console.log("4) el sueldo es: $"+sueldo+" y la cantidad de horas trabajadas es: "+horas+" y el valor por horas es: "+respuesta)
+    
+}
+sueldo();
+
+//5 area de una circunferencia//
+
+function areaCircunferencia(){
+
+    let a = Math.pow(6, 2);
+    let pi = 3.14;
+
+    let respuesta = pi * a;
+
+    console.log("5) el area es: "+respuesta);
+}
+areaCircunferencia();
+
+//6 AREA DE CUADRADO//
+
+function areaCuadrado(){
+
+    let a = 2;
+    let respuesta = Math.pow(a, 2)
+
+    console.log("6) el area del cuadrado es: "+respuesta+" cm");
+}
+areaCuadrado();
+
+//7 HIPOTENUSA DE UN TRIANGULO RECTANGULO//
+
+function hipotenusa(){
+
+    let a = Math.pow(10, 2);
+    let b = Math.pow(12, 2);
+    let c = a + b;
+
+    let respuesta = Math.pow(c, 2);
+    
+    console.log("7) la hipotenusa es: "+respuesta);
+}
+hipotenusa();
+
+//8 CORRIENTE DE UN CIRCUITO SERIE CON 2 RESISTENCIA//
+
+function corriente(){
+
+    let r1 = 8;
+    let r2 = 6;
+    let r3 = 10;
+
+    let respuesta = r1 + r2 + r3;
+
+    console.log("9) la corriente es: "+respuesta);
+}
+corriente();
